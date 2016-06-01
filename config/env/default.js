0ea5531955fa9b6a05dd437aee4f752d1b6c9b08
Garrett Eastham <garrett.eastham@gmail.com>
2016-06-01 14:37:05 -0500

    changed default.js

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'MEAN.JS + AdminLTE',
+    description: 'Full-Stack JavaScript with AdminLTE, MongoDB, Express, AngularJS, and Node.js',
+    keywords: 'adminlte, bootstrap, mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
